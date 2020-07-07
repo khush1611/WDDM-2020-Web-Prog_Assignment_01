f8edb1ea70edd5bbc9cb7d8eb6d589540cfb993f
khush1611 <khushboo.umrigar11@gmail.com>
2020-07-07 19:39:39 -0400

    server side regex validation performed

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,31 @@
 const express =  require("express");
 const router = express.Router();
 
+//fucntions for server-side validations
+//https://www.regextester.com/104030
+// ^                                            Match the beginning of the string
+// a-zA-z                                       capital or small character a-5
+// _-                                           should have _-
+// {3-10}                                       checks for the length b/w 3-10
+function userNameValidation(uName){
+    const regex = /^[a-zA-Z0-9_-]{3,10}$/;
+    return regex.test(uName);
+}
+
+//https://www.ocpsoft.org/tutorials/regular-expressions/password-regular-expression/
+// ^                                            Match the beginning of the string
+// (?=.*[0-9])                                  Require that at least one digit appear anywhere in the string
+// (?=.*[a-z])                                  Require that at least one lowercase letter appear anywhere in the string
+// (?=.*[A-Z])                                  Require that at least one uppercase letter appear anywhere in the string
+// (?=.*[*.!@$%^&(){}[]:;<>,.?~_+-=|\])        Require that at least one special character appear anywhere in the string
+// .{8,32}                                      The password must be at least 8 characters long, but no more than 32
+// $                                            Match the end of the string.
+function passwordValidation(pword){
+    //const regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?~_+-=|\]).{8,32}$/;
+    const regex = /^[A-Za-z0-9]+/;
+    return regex.test(pword);
+}
+
 // login
 router.get("/login",(req, res)=>{
     res.render("users/login",{
@@ -12,15 +37,16 @@ router.get("/login",(req, res)=>{
 router.post('/login-form',(req,res)=>{
 
     const varError =[];
-    
-    if(req.body.username == ""){
-    console.log("true " + req.body.username);
+    const {username, Upassword} = req.body;
+
+    if(username == ""){
+    console.log("true " + username);
         varError.push("you must the username")
     }
     //else{
     //console.log("false " + req.body.username);
     //}
-    if(req.body.password == ""){
+    if(Upassword == ""){
         varError.push("you must enter password")
     }
     if(varError.length > 0){
@@ -45,36 +71,59 @@ router.get("/signup",(req, res)=>{
 router.post('/signup-form',(req,res)=>{
 
     const varError =[];
+    // get form fields in a variable
+    const {name,username,email,address,Upassword, Cpassword} = req.body;
+
+    var form = {
+        nameHolder: req.body.name, 
+        usernameHolder: req.body.username,
+        emailHolder: email,
+        addressHolder: req.body.address,
+    }
+    //console.log(form);
     
-    if(req.body.name == ""){
+    if(name == ""){
         varError.push("you must enter your name")
     }
-
-    if(req.body.username == ""){
+    //console.log(userNameValidation(req.body.username));
+    if(username == ""){
         varError.push("you must enter username")
+    } 
+    else if(!userNameValidation(username))
+    {
+        varError.push("Username can contain characters (3-10), numbers, '-' and '_'. EX: Khush-16");
     }
 
-    if(req.body.email == ""){
+    if(email == ""){
         varError.push("you must enter email")
     }
 
-    if(req.body.address == ""){
+    if(address == ""){
         varError.push("you must enter address")
     }
 
-    if(req.body.password == ""){
+    // console.log(passwordValidation(Upassword));
+    // console.log(Upassword);
+
+    if(Upassword == ""){
         varError.push("you must choose password")
+    } 
+    else if(!passwordValidation(Upassword)){
+        varError.push('Require that at least one digit appear anywhere in the string')
     }
-
-    if(req.body.Cpassword == ""){
-        varError.push("your password doesnt match")
+    if(Cpassword == ""){
+        varError.push("you must enter confirm password")
     }
-
+    if(Upassword !== Cpassword){
+        // console.log("not match");
+        varError.push("password doesn't match")
+    }
     if(varError.length > 0){
         res.render("users/signup", {
             title: "signup",
-            errorMessages: varError
-        });
+            errorMessages: varError,
+            formValues: form
+    });
     }
     else{
         res.redirect("/");
